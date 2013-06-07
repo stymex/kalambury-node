17814abff12d2519c9538496dd5ce0d82c08e799
stymex <stymex@gmail.com>
2013-06-07 17:27:16 +0200

    szkielet rozwiazania + jquery + bootstrap

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'fw34g35gv5h$@G#$G#G$34g34gwe' }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+app.listen(3000, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
