b88d3d2bc849cfaf154ce015d45bf336dc1a2224
stymex <stymex@gmail.com>
2013-06-07 16:46:38 +0200

    naprawa błędu

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-var app = require('express').createServer().listen(3000);
+var express = require('express');
+var app = express();
+
+app.listen(3000);
 
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
