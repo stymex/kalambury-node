546899ce91834555327a81de76f82ffe9062fba4
stymex <stymex@gmail.com>
2013-06-10 11:51:23 +0200

    poprawa błędu w przesyłaniu danych do canvasu

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
     var socket = io.connect('http://localhost:3000');
 
     socket.on('draw', function (data) {
-		if(clients[data.id]){
+		if(data.drawing && clients[data.id]){
 			drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
 		}
         clients[data.id] = data;
@@ -58,12 +58,15 @@ $(document).ready(function(){
     });
 
     $(document).on('mousemove',function(e){
-        if(turn && drawing){
+		if(turn){
 			socket.emit('mousemove',{
 				'x': e.pageX,
 				'y': e.pageY,
+				'drawing': drawing,
 				'id': id
 			});
+		}
+        if(turn && drawing){
             drawLine(prev.x, prev.y, e.pageX, e.pageY);
             prev.x = e.pageX;
             prev.y = e.pageY;
@@ -88,3 +91,80 @@ $(document).ready(function(){
     });
     
 });
+
+
+//~ $(function(){
+//~ 
+    //~ // The URL of your web server (the port is set in app.js)
+    //~ var url = 'http://localhost:3000';
+//~ 
+    //~ var doc = $(document),
+        //~ canvas = $('#area'),
+        //~ ctx = canvas[0].getContext('2d');
+//~ 
+    //~ // A flag for drawing activity
+    //~ var drawing = false;
+//~ 
+    //~ var clients = {};
+//~ 
+//~ 
+    //~ var socket = io.connect(url);
+//~ 
+    //~ socket.on('draw', function (data) {
+//~ 
+        //~ // Is the user drawing?
+        //~ if(data.drawing && clients[data.id]){
+//~ 
+            //~ // Draw a line on the canvas. clients[data.id] holds
+            //~ // the previous position of this user's mouse pointer
+//~ 
+            //~ drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
+        //~ }
+//~ 
+        //~ // Saving the current client state
+        //~ clients[data.id] = data;
+        //~ clients[data.id].updated = $.now();
+    //~ });
+//~ 
+    //~ var prev = {};
+//~ 
+    //~ canvas.on('mousedown',function(e){
+        //~ e.preventDefault();
+        //~ drawing = true;
+        //~ prev.x = e.pageX;
+        //~ prev.y = e.pageY;
+    //~ });
+//~ 
+    //~ doc.bind('mouseup mouseleave',function(){
+        //~ drawing = false;
+    //~ });
+//~ 
+    //~ doc.on('mousemove',function(e){
+        	//~ socket.emit('mousemove',{
+                //~ 'x': e.pageX,
+                //~ 'y': e.pageY,
+                //~ 'drawing': drawing,
+                //~ 'id': id
+            //~ });
+        //~ 
+        //~ if(drawing){
+            //~ drawLine(prev.x, prev.y, e.pageX, e.pageY);
+            //~ prev.x = e.pageX;
+            //~ prev.y = e.pageY;
+        //~ }
+    //~ });
+//~ 
+    //~ var p = $('#area');
+    //~ var position = p.position();
+    //~ 
+	//~ $(window).on('resize', function() {
+		//~ position = p.position();
+	//~ });
+//~ 
+    //~ var drawLine = function (fromx, fromy, tox, toy) {
+        //~ ctx.moveTo(fromx - position.left, fromy - position.top);
+        //~ ctx.lineTo(tox - position.left, toy - position.top);
+        //~ ctx.stroke();
+    //~ }
+//~ 
+//~ });
